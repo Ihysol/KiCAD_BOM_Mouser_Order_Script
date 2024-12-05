00730c819e4ede33efc221a6c032fddfa4cc4150
tgent <tobias.gent@th-deg.de>
2024-12-05 13:44:47 +0100

    layout madness

--- main.py
+++ main.py
@@ -16,22 +16,21 @@ class OrderTool:
         self.gui = None
             
     def process_bom(self, path):
-        pass
-        # print(self.gui.)
-        # success = False
-        # count = 0
         
-        # while count < mouser_config.API_TIMEOUT_MAX_RETRIES and not success:
-        #     client = MouserOrderClient()
-        #     bom_handler = BOMHandler()
-        #     bom_handler.get_bom_files(path)
-        #     bom_handler.process_bom_file(bom_handler.BOM_files[0])
-        #     success = client.order_parts_from_data_array(bom_handler.data_array)
-        #     count+=1
-        #     if not success:
-        #         time.sleep(mouser_config.API_TIMEOUT_SLEEP_S)
+        success = False
+        count = 0
+        
+        while count < mouser_config.API_TIMEOUT_MAX_RETRIES and not success:
+            client = MouserOrderClient()
+            bom_handler = BOMHandler()
+            bom_handler.get_bom_files(path)
+            bom_handler.process_bom_file(bom_handler.BOM_files[0])
+            success = client.order_parts_from_data_array(bom_handler.data_array)
+            count+=1
+            if not success:
+                time.sleep(mouser_config.API_TIMEOUT_SLEEP_S)
 
-        # print(f"tries:{count} - success: {success}")    
+        print(f"tries:{count} - success: {success}")    
         
     def run(self):
         self.gui = GUI(self.process_bom)
