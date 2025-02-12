bd41ab5b8898002d647d0efef9cf347eb841d24f
tgent <tobias.gent@th-deg.de>
2025-02-12 10:19:11 +0100

    added part order multiplier

--- main.py
+++ main.py
@@ -8,11 +8,13 @@ BASE_URL = "https://api.mouser.com/api/v1.0"
 ''' USER VARIABLES - CHANGE VALUES HERE '''
 ENVIRONMENT_VARIABLE_API_KEY_NAME = "MOUSER_API_KEY"    # name of your environment variable (if you need another name for some reason on your system)
 
-CSV_MOUSER_COLUMN_NAME = "MFN"                       # how you named your column
+CSV_MOUSER_COLUMN_NAME = "MNR"                       # how you named your column
 CSV_DELIMITER = ","                                     # the delimiter of your .csv-file
                                     
 API_TIMEOUT_MAX_RETRIES = 10                            # max retries if api decides to return errors -> possibly due to my dataprocessing?
 API_TIMEOUT_SLEEP_S = 2                                 # wait time between retries in seconds
+
+part_order_multiplier = 1                               # multiply by pcbs ordered
 
 ''' mouser API request skeleton '''
 class MouserAPIRequest:
@@ -198,7 +200,7 @@ class MouserOrderClient:
 
         df = pd.DataFrame(data_array)    
         for idx, row in df.iterrows():
-            parts_json.append({'MouserPartNumber': row[headers[0]], 'Quantity': int(row[headers[1]]), 'CustomerPartNumber':row[headers[2]]})
+            parts_json.append({'MouserPartNumber': row[headers[0]], 'Quantity': int(row[headers[1]])*part_order_multiplier, 'CustomerPartNumber':row[headers[2]]})
 
         cart_uuid = uuid.uuid4()
         body = f"{{'CartKey': {cart_uuid}, 'CartItems': {parts_json}}}"  
